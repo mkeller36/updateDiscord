ec319c546957517f98826bb39890590686d8ca25
mkeller@arch <michaelkeller1997@gmail.com>
2025-12-13 10:30:33 -0500

    changed for discord Canary

--- update_discord.sh
+++ update_discord.sh
@@ -3,6 +3,6 @@
 cd /home/mkeller/Downloads/
 wget "https://discordapp.com/api/download/canary?platform=linux&format=tar.gz" -O discord.tar.gz
 tar -xf /home/mkeller/Downloads/discord.tar.gz
-sudo rm -rf /sbin/Discord/
-sudo mv Discord/ /sbin/Discord/
+sudo rm -rf /sbin/DiscordCanary/
+sudo mv DiscordCanary/ /sbin/DiscordCanary/
 sudo rm /home/mkeller/Downloads/discord.tar.gz
